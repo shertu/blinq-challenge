ce35f9a2aea37f062cb27ffd7938ec1954778d59
shertu <djared.xeknau@outlook.com>
2022-03-31 12:33:58 +1100

    Updated packages to latest versions

--- apps/client-web/next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  experimental: {
-    outputStandalone: true,
-  },
-  images: {
-    domains: ['i.imgur.com', 'localhost', 'zukte21.blob.core.windows.net'],
-  },
-};
--- apps/client-web/src/pages/index.tsx
+++ apps/client-web/src/pages/index.tsx
@@ -1,28 +1,16 @@
 import {Typography} from '@mui/material';
 import {AppPage} from 'components';
 
-import Head from 'next/head';
 import React from 'react';
 
 /**
  * The page the user scrolls down to after seeing the landing screen.
  */
 export function LandingPage() {
-  const landingScreenScrollToId = 'about-me-content';
-
   return (
-    <>
-      <Head>
-        <title>Jared Michael Blackman</title>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js" />
-      </Head>
-      <AppPage
-        className="min-h-screen pt-office-word px-6"
-        id={landingScreenScrollToId}
-      >
-        <Typography>Apple</Typography>
-      </AppPage>
-    </>
+    <AppPage>
+      <Typography>Apple</Typography>
+    </AppPage>
   );
 }
 
